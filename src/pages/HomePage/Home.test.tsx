65862aff443b311a27fd201ef19eb063bd81a284
Gabriel Porto <devgarb.fs@gmail.com>
2024-04-24 17:04:24 -0300

    jest and react testing library update

--- src/pages/HomePage/Home.test.tsx
+++ src/pages/HomePage/Home.test.tsx
@@ -1,12 +1,12 @@
 // react testing library
 
 import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import { Home } from './Home';
 
 describe("<Home />", () => {
     it("Renderizou o título", () => {
         render(<Home />);
 
-        expect(screen.getByText("Home")).toBeInTheDocument();
+        expect(screen.getByText("OKAY")).toBeInTheDocument();
     });
 });
--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,5 +1,7 @@
-import { Button } from "../../components/Button/Button";
 import styles from "./Home.module.css";
+// import { Button } from "../../components/Button/Button";
+// import { Link } from 'react-router-dom'
+// import { ROUTES } from '../../routes'
 
 // import {
 //   Banner,
@@ -7,13 +9,15 @@ import styles from "./Home.module.css";
 //   Hero,
 //   ProductsList,
 // } from '../../components' 
-
 // => Exemplo de como deve chamar os componentes para a página.
 
 export const Home = () => {
   return (
 <div className={styles.container}>
-    <Button text="BOTÃO" />
+  <h1>OKAY</h1>
+  {/* <Link to={ROUTES.Home}>
+    <Button text="Botão" />
+  </Link> */}
     </div>
   );
 };
